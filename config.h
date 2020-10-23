a9b035b75686bacd651e7a63866cbd0303f382bf
Keegan K <keegan@NAVI>
2020-10-22 22:51:44 -0400

    font

--- config.h
+++ config.h
@@ -5,8 +5,9 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Fira Code:pixelsize=16:antialias=true:autohint=true";
-static char *font2[] = { "M+ 1m:pixelsize=18:antialias=true:autohint=true", "JoyPixels:pixelsize=16:antialias=true:autohint=true" };
+//static char *font = "Fira Code:pixelsize=16:antialias=true:autohint=true";
+static char *font = "Unifont:pixelsize=16:antialias=true:autohint=true";
+static char *font2[] = { /*"M+ 1m:pixelsize=18:antialias=true:autohint=true",*/ "JoyPixels:pixelsize=16:antialias=true:autohint=true" };
 static int borderpx = 2;
 
 /*
